01071931573ef62c5069e5e7c43596002b51f162
camorra-skk <sumtsom@gmail.com>
2018-08-14 18:02:57 +0530

    Latest Fix

---
+++ src/main/java/com/example/entity/LoginResponse.java
@@ -0,0 +1,26 @@
+package com.example.entity;
+
+public class LoginResponse {
+String loginkey;
+String uid;
+String usertype;
+public String getLoginkey() {
+	return loginkey;
+}
+public void setLoginkey(String loginkey) {
+	this.loginkey = loginkey;
+}
+public String getUid() {
+	return uid;
+}
+public void setUid(String uid) {
+	this.uid = uid;
+}
+public String getUsertype() {
+	return usertype;
+}
+public void setUsertype(String usertype) {
+	this.usertype = usertype;
+}
+
+}
